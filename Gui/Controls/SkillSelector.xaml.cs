020eaeb3aca4cdb64da0895cdf01715768b8780a
lilith72 <60119376+lilith72@users.noreply.github.com>
2021-05-07 15:25:48 -0700

    Selectors for skills, slots, and sorts now reusable controls (#19)

---
+++ Gui/Controls/SkillSelector.xaml.cs
@@ -0,0 +1,98 @@
+﻿using JustinsASS.Engine.Contract.DataModel;
+using JustinsASS.Engine.Contract.FrontEndInterface;
+using JustinsASS.Gui.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JustinsASS.Gui.Controls
+{
+    /// <summary>
+    /// Interaction logic for SkillSelector.xaml
+    /// </summary>
+    public partial class SkillSelector : UserControl
+    {
+        public IDictionary<string, int> SelectedSkills {
+            get
+            {
+                IDictionary<string, int> selectedSkills = new Dictionary<string, int>();
+                selectedSkills = mSkills.ToDictionary(x => x.Key, x => x.Value.Level);
+                return selectedSkills;
+            }
+            private set {}
+        }
+        private IDictionary<string, SkillLvlMax> mSkills;
+
+        public SkillSelector(ASS ass)
+        {
+            InitializeComponent();
+            this.SelectedSkills = new Dictionary<string, int>();
+            mSkills = new Dictionary<string, SkillLvlMax>();
+
+            foreach (KeyValuePair<string, int> skill in ass.GetSkillNamesToMaxLevelMapping())
+            {
+                mSkills.Add(skill.Key, new SkillLvlMax(0, skill.Value));
+            }
+            UpdateSkills();
+        }
+
+        private void UpdateSkills()
+        {
+            // Update dropdown
+            cbAddSkill.ItemsSource = mSkills.Where(s => s.Value.Level == 0).OrderBy(s => s.Key).Select(s2 => s2.Key).ToList();
+            // Update List of Skills
+            lvSkillList.ItemsSource = mSkills.Where(s => s.Value.Level > 0);
+        }
+
+        private void OnClick_AddSkill(object sender, RoutedEventArgs e)
+        {
+            if (cbAddSkill.SelectedItem != null)
+            {
+                string skill = cbAddSkill.SelectedItem.ToString();
+                if (mSkills.ContainsKey(skill))
+                {
+                    mSkills[skill].Level = 1;
+                    UpdateSkills();
+                }
+            }
+        }
+
+        private void OnKeyDown_AddSkill(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                OnClick_AddSkill(null, null);
+            }
+        }
+
+        private void OnClick_RemoveSkill(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            string skill = button.Tag.ToString();
+
+            if (cbAddSkill.SelectedItem != null)
+            {
+                mSkills[skill].Level = 0;
+            }
+            UpdateSkills();
+        }
+
+        private void OnChange_SkillValue(object sender, RoutedEventArgs e)
+        {
+            var upcSkill = (UpDownControl)sender;
+            string skillID = upcSkill.Tag.ToString();
+            mSkills[skillID].Level = upcSkill.Value;
+        }
+    }
+}
--- Gui/Windows/NewTalismanWindow.xaml.cs
+++ Gui/Windows/NewTalismanWindow.xaml.cs
@@ -25,8 +25,8 @@ namespace JustinsASS.Gui.Windows
     /// </summary>
     public partial class NewTalismanWindow : Window
     {
-        private IDictionary<string, int> mSkills = new Dictionary<string, int>();
-        int[] mDecoSlots = new int[Helper.MAX_SLOTS];
+        SlotSelector mSlotSelector;
+        SkillSelector mSkillSelector;
         ASS mAss;
 
         public SkillContributor Result { set; get; }
@@ -35,82 +35,22 @@ namespace JustinsASS.Gui.Windows
         {
             InitializeComponent();
             mAss = ass;
-            cbAddSkill.ItemsSource = mAss.GetSkillNamesToMaxLevelMapping().Keys;
-            for (int i = 0; i < Helper.MAX_SLOT_SIZE; i++)
-            {
-                Label newLabel = new Label();
-                newLabel.Content = "Size " + (i + 1) + " Slots";
-                UpDownControl newUdc = new UpDownControl();
-                newUdc.Min = 0;
-                newUdc.Max = Helper.MAX_SLOTS;
-                newUdc.Value = 0;
-                newUdc.Tag = i.ToString();
-                newUdc.ValueChanged += OnChange_SlotValue;
 
-                spSlots.Children.Add(newLabel);
-                spSlots.Children.Add(newUdc);
-            }
-        }
+            mSkillSelector = new SkillSelector(ass);
+            gridChoosers.Children.Add(mSkillSelector);
+            Grid.SetColumn(mSkillSelector, 0);
+            Grid.SetRow(mSkillSelector, 0);
 
-        private void OnClick_AddSkill(object sender, RoutedEventArgs e)
-        {
-            if (cbAddSkill.SelectedItem != null)
-            {
-                string skill = cbAddSkill.SelectedItem.ToString();
-                if (!mSkills.ContainsKey(skill))
-                {
-                    mSkills.Add(skill, 1);
-                    UpdateSkills();
-                }
-            }
-        }
-        private void OnKeyDown_AddSkill(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                OnClick_AddSkill(null, null);
-            }
-        }
-
-        private void OnClick_RemoveSkill(object sender, RoutedEventArgs e)
-        {
-            var button = (Button)sender;
-            string skill = button.Tag.ToString();
-
-            mSkills.Remove(skill);
-            UpdateSkills();
-        }
-        private void OnChange_SkillValue(object sender, RoutedEventArgs e)
-        {
-            var upcSkill = (UpDownControl)sender;
-            string skillID = upcSkill.Tag.ToString();
-            mSkills[skillID] = upcSkill.Value;
-        }
-
-        private void OnChange_SlotValue(object sender, RoutedEventArgs e)
-        {
-            var upcSlot = (UpDownControl)sender;
-            int slotNum = Int32.Parse(upcSlot.Tag.ToString());
-
-            mDecoSlots[slotNum] = upcSlot.Value;
-        }
-        private void UpdateSkills()
-        {
-            IList<SkillValue> skillList = new List<SkillValue>();
-            lvSkillList.ItemsSource = null;
-            foreach (KeyValuePair<string, int> skill in mSkills.OrderBy(key => key.Key))
-            {
-                skillList.Add(new SkillValue(skill.Key, skill.Value));
-            }
-            IDictionary<string, SkillLvlMax> skills = Helper.GetSkillsWithMax(skillList, mAss);
-            lvSkillList.ItemsSource = null;
-            lvSkillList.ItemsSource = skills;
+            mSlotSelector = new SlotSelector(Helper.MAX_SLOT_SIZE, Helper.MAX_SLOTS);
+            gridChoosers.Children.Add(mSlotSelector);
+            Grid.SetColumn(mSlotSelector, 2);
+            Grid.SetRow(mSlotSelector, 0);
         }
 
         private void OnClick_AddTalisman(object sender, RoutedEventArgs e)
         {
-            IList<int> decoSlots = Helper.DecorationArrayToList(mDecoSlots);
-            IList<SkillValue> skillList = mSkills.Select(kvp => new SkillValue(kvp.Key, kvp.Value)).ToList(); ;
+            IList<int> decoSlots = Helper.DecorationArrayToList(mSlotSelector.SelectedSlots);
+            IList<SkillValue> skillList = mSkillSelector.SelectedSkills.Select(kvp => new SkillValue(kvp.Key, kvp.Value)).ToList(); ;
             SkillContributor newTalisman = new SkillContributor(tbTalismanName.Text, 0, decoSlots, ArmorSlot.Talisman, skillList);
             this.Result = newTalisman;
             this.DialogResult = true;
